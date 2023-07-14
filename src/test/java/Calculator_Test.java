a65f19ec10072e559d499e6c897b6cba6166d153
Nevzat <nevzatcelik151907@gmail.com>
2023-07-14 20:52:11 +0300

    second lesson

--- src/test/java/Calculator_Test.java
+++ src/test/java/Calculator_Test.java
@@ -5,6 +5,7 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.aspectj.weaver.ast.And;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -19,15 +20,27 @@ AndroidDriver<AndroidElement> driver; // Android cizahlar icin kullanilmasi gere
 
     @Test
     public void test1() throws MalformedURLException {
+        // kullanici gerekli kurulumlari yapar
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 2");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2"); // Eger ki kullandigimiz android surumu 6 veya ustuyse UiAutomator2 kullanilir
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\APPIUM_T-113\\Apps\\Calculator_8.4.1 (520193683)_Apkpure.apk"); // Eger ki kullandigimiz android surumu 6 veya ustuyse UiAutomator2 kullanilir
-
+        // Eger ki kullandigimiz android surumu 6 veya ustuyse UiAutomator2 kullanilir
+       // capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\APPIUM_T-113\\Apps\\Calculator_8.4.1 (520193683)_Apkpure.apk");
+         // app capabilityType bir uygulamayi yuklemek istedigimizde indirdigimiz apk dosyasinin path uzantisini vererek o uygulamayi yuklemek icin kullandigimiz desiredCapability
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+
+        driver.activateApp("com.google.android.calculator");
+        // uygulamanin yuklendigini dogrular(isInstalled)
+        Assert.assertTrue(driver.isAppInstalled("com.google.android.calculator"));
+        // uygulamanin acildigini dogrular
+        // carpma,bolme,toplama,cikarma islemleri yaparak sonuclari dogrular
+        // AC butonuna tiklayarak ana ekrani temizler
+
 
     }
 
