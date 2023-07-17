7ae7e03b9ff1bc5da4b7eb427ab8e6ef9e672db4
Nevzat <nevzatcelik151907@gmail.com>
2023-07-17 21:03:11 +0300

    forth lesson

--- src/test/java/tests/DAY_2/AppYukleme.java
+++ src/test/java/tests/DAY_2/AppYukleme.java
@@ -17,12 +17,12 @@ public class AppYukleme {
     public void upload() throws MalformedURLException {
         // kullanici gerekli kurulumlari yapar
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 2");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2"); // Eger ki kullandigimiz android surumu 6 veya ustuyse UiAutomator2 kullanilir
         // Eger ki kullandigimiz android surumu 6 veya ustuyse UiAutomator2 kullanilir
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\APPIUM_T-113\\Apps\\arabam.com_4.8.0_Apkpure.apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\ahmet\\IdeaProjects\\APPIUM_T-113\\Apps\\Apk Bilgisi_2.3.4_apkcombo.com.apk");
         // app capabilityType bir uygulamayi yuklemek istedigimizde indirdigimiz apk dosyasinin path uzantisini vererek o uygulamayi yuklemek icin kullandigimiz desiredCapability
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
